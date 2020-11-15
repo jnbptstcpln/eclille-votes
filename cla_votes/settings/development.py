5b2802ced5a49ca940740a683fbeee871d8e230c
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-11-15 20:28:21 +0100

    Utilisation de MySQL en base de données de développement

--- cla_votes/settings/development.py
+++ cla_votes/settings/development.py
@@ -17,3 +17,18 @@ LOGGING = {
         'level': 'DEBUG',
     },
 }
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'host': config("DATABASE_HOST", "127.0.0.1"),
+            'port': int(config("DATABASE_PORT", "3306")),
+            'user': config("DATABASE_USER"),
+            'passwd': config("DATABASE_PASSWORD"),
+            'db': config("DATABASE_NAME"),
+        },
+    }
+}
