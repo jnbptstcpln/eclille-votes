35e1672191623ca23ff6540fa5365a40c880e24a
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-11-15 13:54:41 +0100

    Extension du modèle django.contrib.auth.models.User à l'aide du modèle UserInfos

--- cla_auth/models.py
+++ cla_auth/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Test(models.Model):
-    name = models.CharField(max_length=15)
+class UserInfos(models.Model):
+    """
+        The UserInfo model extends user by adding new properties
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="infos")
+    promo = models.IntegerField()
