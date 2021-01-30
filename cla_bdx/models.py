f5252135a25ed95c89e0ab88baad689894b2a683
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-01-30 23:15:28 +0100

    Calcul de l'ann

--- cla_bdx/models.py
+++ cla_bdx/models.py
@@ -7,6 +7,24 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 
 
+class FilePath:
+
+    @classmethod
+    def _path(cls, instance, pathlist, filename):
+        ext = filename.split('.')[-1]
+        filename = "%s-%s.%s" % (slugify(instance.name), uuid.uuid4(), ext)
+        pathlist.append(filename)
+        return os.path.join(*pathlist)
+
+    @classmethod
+    def program(cls, instance, filename):
+        return cls._path(instance, ["cla_bdx", "program"], filename)
+
+    @classmethod
+    def logo(cls, instance, filename):
+        return cls._path(instance, ["cla_bdx", "logo"], filename)
+
+
 class Campaign(models.Model):
 
     class Meta:
@@ -23,6 +41,12 @@ class Campaign(models.Model):
     ends_on = models.DateTimeField()
     type = models.CharField(max_length=3, choices=BDX.choices, verbose_name="Campagne")
 
+    @property
+    def school_year(self):
+        if 1 <= self.starts_on.month < 9:
+            return self.starts_on.year-1
+        return self.starts_on.year
+
     def __str__(self):
         return f"Campagne {self.type.upper()} {self.starts_on.year}"
 
@@ -32,27 +56,11 @@ class List(models.Model):
     class Meta:
         verbose_name = "Liste"
 
-    class FilePath:
-
-        @classmethod
-        def _path(cls, instance, pathlist, filename):
-            ext = filename.split('.')[-1]
-            filename = "%s-%s.%s" % (slugify(instance.name), uuid.uuid4(), ext)
-            pathlist.append(filename)
-            return os.path.join(*pathlist)
-
-        @classmethod
-        def program(cls, instance, filename):
-            return cls._path(instance, ["cla_bdx", "program"], filename)
-
-        @classmethod
-        def logo(cls, instance, filename):
-            return cls._path(instance, ["cla_bdx", "logo"], filename)
-
     campaign = models.ForeignKey(Campaign, related_name="lists", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     logo = ResizedImageField(size=[500, 500], quality=90, upload_to=FilePath.logo, force_format="PNG", null=True, blank=True)
     program = models.FileField(upload_to=FilePath.program, null=True, blank=True)
+    votes_binary = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.name} - {self.campaign.type.upper()} {self.campaign.starts_on.year}"
@@ -69,19 +77,6 @@ class Vote(models.Model):
 
     def __str__(self):
         return f"Campagne {self.campaign.type.upper()} {self.starts_on.year}"
-
-
-class VoteUrn(models.Model):
-
-    class Meta:
-        verbose_name = "Urne"
-
-    vote = models.ForeignKey(Vote, related_name="urns", on_delete=models.CASCADE)
-    list = models.OneToOneField(List, related_name="urn", on_delete=models.CASCADE)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.list.name} - {self.campaign.type.upper()} {self.campaign.starts_on.year}"
 
 
 class VoteUser(models.Model):
