bafeb5243046a433a18ef1bc6d9f733b0837d08a
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2024-01-22 21:45:06 +0100

    Update url filter

--- cla_bdx/urls.py
+++ cla_bdx/urls.py
@@ -4,8 +4,12 @@ from .views import *
 app_name = "cla_bdx"
 urlpatterns = [
     path("bdx", CurrentCampaignView.as_view(), name="current"),  # Current campaigns
-    re_path("election/(?P<type>bd[a-z])", CampaignVoteView.as_view(), name="vote"),
+    re_path("election/(?P<type>[a-z_]+)", CampaignVoteView.as_view(), name="vote"),
     path("bdx/<int:campaign_pk>/box", CampaignVoteBoxView.as_view(), name="box"),
-    path("bdx/<int:campaign_pk>/calendar", CampaignCalendarView.as_view(), name="calendar"),
+    path(
+        "bdx/<int:campaign_pk>/calendar",
+        CampaignCalendarView.as_view(),
+        name="calendar",
+    ),
     path("bdx/archives", LastCampaignView.as_view(), name="last"),  # All campaign list
 ]
