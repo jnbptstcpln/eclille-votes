0640ededa4e6c7b309cc0b544938fadabc091487
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2024-03-05 08:24:25 +0100

    Add election to index

--- cla_public/views.py
+++ cla_public/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views.generic import View
 
 from cla_bdx.models import Campaign
+from cla_ca.models import Election
 
 
 class AbstractPublicView(View):
 
     def context(self, context=dict()):
-        context['active_navigation'] = context.get('active_navigation', "accueil")
+        context["active_navigation"] = context.get("active_navigation", "accueil")
         return context
 
 
@@ -16,12 +17,16 @@ class IndexPublicView(AbstractPublicView):
     def get(self, req):
         on_going_campaign = Campaign.objects.ongoing()
         on_going_campaign_vote = Campaign.objects.vote_ongoing()
+        on_going_election = Election.objects.ongoing()
 
         return render(
             req,
             "cla_public/index.html",
-            self.context({
-                'on_going_campaign': on_going_campaign,
-                'on_going_campaign_vote': on_going_campaign_vote
-            })
+            self.context(
+                {
+                    "on_going_campaign": on_going_campaign,
+                    "on_going_campaign_vote": on_going_campaign_vote,
+                    "on_going_election": on_going_election,
+                }
+            ),
         )
