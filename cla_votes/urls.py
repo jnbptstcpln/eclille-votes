7b312d2b1f4a3feec48ea926fa41b8e21269f7c7
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2024-03-05 08:13:40 +0100

    Update order

--- cla_votes/urls.py
+++ cla_votes/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,14 +22,16 @@ from django.conf.urls.static import static
 from cla_auth.views import LoginAuthView
 
 urlpatterns = [
-    path("admin/login/", LoginAuthView.as_view(), name='admin:login'),  # Override default login
-    path('admin/', admin.site.urls),
-    path('auth/', include("cla_auth.urls")),
-    path('', include("cla_public.urls")),
-    path('', include("cla_bdx.urls")),
-    path('', include("cla_ca.urls")),
-    path('', include("cla_customvotes.urls")),
-    path('enscl/', include("cla_enscl.urls")),
+    path(
+        "admin/login/", LoginAuthView.as_view(), name="admin:login"
+    ),  # Override default login
+    path("admin/", admin.site.urls),
+    path("auth/", include("cla_auth.urls")),
+    path("", include("cla_public.urls")),
+    path("", include("cla_ca.urls")),
+    path("", include("cla_bdx.urls")),
+    path("", include("cla_customvotes.urls")),
+    path("enscl/", include("cla_enscl.urls")),
 ]
 
 if settings.DEBUG:
@@ -36,7 +39,7 @@ if settings.DEBUG:
 
 admin.site.site_header = "Site des votes"
 
-handler400 = 'cla_votes.error_views.error_400'
-handler403 = 'cla_votes.error_views.error_403'
-handler404 = 'cla_votes.error_views.error_404'
-handler500 = 'cla_votes.error_views.error_500'
+handler400 = "cla_votes.error_views.error_400"
+handler403 = "cla_votes.error_views.error_403"
+handler404 = "cla_votes.error_views.error_404"
+handler500 = "cla_votes.error_views.error_500"
